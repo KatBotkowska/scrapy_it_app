283b87cce6e905c552efab0ad5d47bbaecc772e6
KasiaBotkowska <katarzyna.botkowska@gmail.com>
2020-10-24 19:00:52 +0200

    apscheduler on Heroku

---
+++ clock.py
@@ -0,0 +1,15 @@
+#Python scheduler on Heroku https://devcenter.heroku.com/articles/clock-processes-python
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=19)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+
+sched.start()
