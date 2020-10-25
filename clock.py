49c6122325c1e75c03a30027ff1029ebc7391b09
KasiaBotkowska <katarzyna.botkowska@gmail.com>
2020-10-25 20:46:36 +0100

    scheduler for spider on Heroku

--- clock.py
+++ clock.py
@@ -1,15 +1,27 @@
 #Python scheduler on Heroku https://devcenter.heroku.com/articles/clock-processes-python
 
 from apscheduler.schedulers.blocking import BlockingScheduler
+from scrapy_job_it.spiders.bdcrawler import BDcrawlerSpider
+from scrapy.utils.project import get_project_settings
+from scrapy.crawler import CrawlerProcess
+from apscheduler.schedulers.twisted import TwistedScheduler
 
-sched = BlockingScheduler()
+process = CrawlerProcess(get_project_settings())
+process.crawl(BDcrawlerSpider)
+process.start()
+scheduler = TwistedScheduler()
+scheduler.add_job(process.crawl, 'cron', args=[BDcrawlerSpider], hour=20, minute=50)
+scheduler.start()
+process.start(False)
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=19)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
-
-sched.start()
+# sched = BlockingScheduler()
+#
+# @sched.scheduled_job('interval', minutes=3)
+# def timed_job():
+#     print('This job is run every three minutes.')
+#
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=19)
+# def scheduled_job():
+#     print('This job is run every weekday at 5pm.')
+#
+# sched.start()
