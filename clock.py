287c8998282d4539adca8be0394460bb8a7564aa
KasiaBotkowska <katarzyna.botkowska@gmail.com>
2020-11-09 21:57:02 +0100

    function for sheduler spider with spider name and hour

--- clock.py
+++ clock.py
@@ -4,18 +4,34 @@
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from scrapy_job_it.spiders.bdcrawler import BDcrawlerSpider
+from scrapy_job_it.spiders.jjcrawler import JjcrawlerSpider
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
 from apscheduler.schedulers.twisted import TwistedScheduler
+#working version for BDcrawlerSpider
+# process = CrawlerProcess(get_project_settings())
+# process.crawl(BDcrawlerSpider)
+# process.start()
+# scheduler = TwistedScheduler()
+# scheduler.add_job(process.crawl, 'cron', args=[BDcrawlerSpider], hour=20, minute=50)
+# scheduler.start()
+# process.start(False)
 
-process = CrawlerProcess(get_project_settings())
-process.crawl(BDcrawlerSpider)
-process.start()
-scheduler = TwistedScheduler()
-scheduler.add_job(process.crawl, 'cron', args=[BDcrawlerSpider], hour=20, minute=50)
-scheduler.start()
-process.start(False)
+#version for 2 spiders scheduled in different houres
+def run_spider(spider, hour):
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(spider)
+    process.start()
+    scheduler = TwistedScheduler()
+    scheduler.add_job(process.crawl, 'cron', args=[spider], hour=hour, minute=50)
+    scheduler.start()
+    process.start(False)
 
+
+run_spider(BDcrawlerSpider, 20)
+run_spider(JjcrawlerSpider, 10)
+
+#run scheduler - tutorial
 # sched = BlockingScheduler()
 #
 # @sched.scheduled_job('interval', minutes=3)
